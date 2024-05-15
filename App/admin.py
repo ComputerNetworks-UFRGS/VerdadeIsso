b9ba3244ff1d5cac2cad3f6c68bbaa775b4d694f
Muriel F. Franco <mffranco@inf.ufrgs.br>
2024-05-15 16:12:22 -0300

    Adicionado modelos no painel de admin e criado superuser

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UploadedFile, CheckedFile, Sources # Import your models
 
 # Register your models here.
+admin.site.register(UploadedFile)
+admin.site.register(CheckedFile)
+admin.site.register(Sources)
+

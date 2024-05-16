8974efe4395ce27a469840d6d34b0eccad085f8a
ArthurCamargo <arthurcamargo13@gmail.com>
2024-05-16 17:46:42 -0300

    fix some problems with urls.py

--- App/urls.py
+++ App/urls.py
@@ -6,13 +6,11 @@ urlpatterns = [
     path('upload/', views.upload_file, name='upload_file'),
     path('upload/success/', views.upload_file, name='upload_file_success'),
     path('data/', views.data_dump, name='data_dump'),
-<<<<<<< HEAD
     path('', views.home_page, name='home_page'),
     path('collaborator_dashboard/', views.home_page, name='index.html'),
     path('login/', views.home_page, name='index.html'),
     path('dashboard/', views.home_page, name='index.html'),
     path('report/', views.home_page, name='index.html'),
-=======
     path('', views.index, name='index'),
 
 ]
